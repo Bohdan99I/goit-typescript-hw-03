19094542ba565f82488d490e76d8dccdb5eb5896
Bohdan99I <bohdan.voiyov@gmail.com>
2023-12-29 18:57:54 +0200

    small_fix

--- src/4.ts
+++ src/4.ts
@@ -42,12 +42,10 @@ class MyHouse extends House {
 }
 
 const key = new Key();
-
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
 export { };
